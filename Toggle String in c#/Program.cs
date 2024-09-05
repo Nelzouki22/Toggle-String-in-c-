d4f5cb71692ae356a2d530141b236304c530ca6b
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-09-05 17:50:35 -0400

    Add project files.

---
+++ Toggle String in c#/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // قراءة السلسلة النصية
+        string S = Console.ReadLine();
+
+        // تغيير حالة الأحرف
+        char[] result = new char[S.Length];
+        for (int i = 0; i < S.Length; i++)
+        {
+            if (char.IsLower(S[i]))
+            {
+                result[i] = char.ToUpper(S[i]);
+            }
+            else if (char.IsUpper(S[i]))
+            {
+                result[i] = char.ToLower(S[i]);
+            }
+        }
+
+        // طباعة السلسلة الناتجة
+        Console.WriteLine(new string(result));
+    }
+}
+
